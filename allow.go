f05364051e1a99f4072bc5416bc724840b41966a
Maxwell Munakata <maxkat@shadown.local>
2018-09-19 21:28:11 -0600

    very first program running with Add New Child Button

---
+++ allow.go
@@ -0,0 +1,54 @@
+// Copyright (c) 2018, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"github.com/goki/gi"
+	"github.com/goki/gi/gimain"
+	"github.com/goki/gi/oswin"
+)
+
+func main() {
+	gimain.Main(func() {
+		mainrun()
+	})
+}
+
+func mainrun() {
+	width := 1024
+	height := 768
+	win := gi.NewWindow2D("allow", "Allowance App", width, height, true)
+
+	vp := win.WinViewport2D()
+	updt := vp.UpdateStart()
+
+	mfr := win.SetMainFrame()
+
+	but := mfr.AddNewChild(gi.KiT_Button, "button").(*gi.Button)
+	but.SetText("Add New Child")
+
+	// main menu
+	appnm := oswin.TheApp.Name()
+	mmen := win.MainMenu
+	mmen.ConfigMenus([]string{appnm, "Edit", "Window"})
+
+	amen := win.MainMenu.KnownChildByName(appnm, 0).(*gi.Action)
+	amen.Menu = make(gi.Menu, 0, 10)
+	amen.Menu.AddAppMenu(win)
+
+	emen := win.MainMenu.KnownChildByName("Edit", 1).(*gi.Action)
+	emen.Menu = make(gi.Menu, 0, 10)
+	emen.Menu.AddCopyCutPaste(win)
+
+	win.OSWin.SetCloseCleanFunc(func(w oswin.Window) {
+		go oswin.TheApp.Quit() // once main window is closed, quit
+	})
+
+	win.MainMenuUpdated()
+
+	vp.UpdateEndNoSig(updt)
+
+	win.StartEventLoop()
+}
